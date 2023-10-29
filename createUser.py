8803ee48cb353b856b907e5e4b5c399d3d7c9784
antenor-z <antenor-z@pm.me>
2023-10-29 16:15:41 -0300

    Make compatible with the new db_exec()

--- createUser.py
+++ createUser.py
@@ -5,7 +5,7 @@ from getpass import getpass
 def create_user(username, password):
     (password_salt, password_hash) = hash_password(password)
     db_exec(f"""INSERT INTO user (username, pass_salt, pass_hash) 
-            VALUES ('{username}', '{password_salt}', '{password_hash}');""")
+            VALUES ('{username}', '{password_salt}', '{password_hash}');""", ())
     
 if __name__ == "__main__":
     try:
